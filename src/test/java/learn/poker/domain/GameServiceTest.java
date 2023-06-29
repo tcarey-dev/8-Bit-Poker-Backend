e2fc243b6295efd4bb534b52b8faa2500ff86142
aaronsocks <aasaccurato@gmail.com>
2023-06-28 19:50:32 -0700

    completed game service tests

--- src/test/java/learn/poker/domain/GameServiceTest.java
+++ src/test/java/learn/poker/domain/GameServiceTest.java
@@ -43,7 +43,6 @@ public class GameServiceTest {
 
     @Test
     void shouldAddGame() {
-
         Player player1 = new Player("fun@testing.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
         player1.setPlayerId(0);
 
@@ -64,7 +63,6 @@ public class GameServiceTest {
         Result<Game> actual = service.add(game);
         assertTrue(actual.isSuccess());
         assertEquals(expectedGame, actual.getPayload());
-
     }
 
     @Test
@@ -84,37 +82,118 @@ public class GameServiceTest {
 
         assertFalse(result.isSuccess());
         assertEquals(1, result.getMessages().size());
+        assertEquals(ResultType.INVALID, result.getType());
         assertEquals("pot must be greater than or equal to zero", result.getMessages().get(0));
     }
 
     @Test
     void shouldNotAddGameWithLessThanTwoPlayers() {
+        Player player1 = new Player("fun@testing.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player1.setPlayerId(0);
 
+        Game game = new Game();
+        game.setPot(250);
+        game.setPlayers(List.of(player1));
+
+        Result<Game> result = service.add(game);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getMessages().size());
+        assertEquals(ResultType.INVALID, result.getType());
+        assertEquals("two players are required to start a game", result.getMessages().get(0));
     }
 
     @Test
     void shouldUpdateGame() {
+        Player player1 = new Player("fun@testing.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player1.setPlayerId(0);
 
+
+        Player player2 = new Player("john@smith.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player2.setPlayerId(1);
+
+        Board board = new Board(
+                1,
+                List.of(Card.ACE_OF_CLUBS,
+                        Card.ACE_OF_DIAMONDS,
+                        Card.ACE_OF_SPADES),
+                Card.EIGHT_OF_CLUBS,
+                Card.ACE_OF_HEARTS);
+
+        Game toUpdate = new Game(1, 350,"lisa@simpson.com", board, List.of(player1, player2));
+
+        toUpdate.setPot(200);
+
+        when(repository.update(toUpdate)).thenReturn(true);
+
+        Result<Game> actual = service.update(toUpdate);
+        assertTrue(actual.isSuccess());
+        assertEquals(0, actual.getMessages().size());
     }
 
     @Test
     void shouldNotUpdateGameWithInvalidPot() {
+        Player player1 = new Player("fun@testing.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player1.setPlayerId(0);
 
+
+        Player player2 = new Player("john@smith.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player2.setPlayerId(1);
+
+
+        Game game = new Game();
+        game.setPot(350);
+        game.setPlayers(List.of(player1, player2));
+
+        game.setPot(-99);
+
+        Result<Game> actual = service.update(game);
+
+        assertFalse(actual.isSuccess());
+        assertEquals(1, actual.getMessages().size());
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertEquals("pot must be greater than or equal to zero", actual.getMessages().get(0));
     }
 
     @Test
     void shouldNotUpdateGameWithLessThanTwoPlayers() {
+        Player player1 = new Player("fun@testing.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player1.setPlayerId(0);
 
+
+        Player player2 = new Player("john@smith.com", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa");
+        player2.setPlayerId(1);
+
+
+        Board board = new Board(
+                1,
+                List.of(Card.ACE_OF_CLUBS,
+                        Card.ACE_OF_DIAMONDS,
+                        Card.ACE_OF_SPADES),
+                Card.EIGHT_OF_CLUBS,
+                Card.ACE_OF_HEARTS);
+
+        Game game = new Game(1, 350,"lisa@simpson.com", board, List.of(player1, player2));
+
+        game.setPlayers(List.of(player1));
+
+        Result<Game> result = service.update(game);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getMessages().size());
+        assertEquals(ResultType.INVALID, result.getType());
+        assertEquals("two players are required to start a game", result.getMessages().get(0));
     }
 
     @Test
     void shouldDeleteById() {
-
+        when(repository.delete(1)).thenReturn(true);
+        assertTrue(service.deleteById(1));
     }
 
     @Test
     void shouldNotDeleteByUnknownId() {
-
+        assertFalse(service.deleteById(99999));
     }
 
 }
