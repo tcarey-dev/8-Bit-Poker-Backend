3da096cb22b55835fc88e8003eb6b5acf1432789
aaronsocks <aasaccurato@gmail.com>
2023-06-30 07:26:05 -0700

    fixed merge conflicts

--- sql/8-bit-poker-schema-test.sql
+++ sql/8-bit-poker-schema-test.sql
@@ -11,24 +11,25 @@ create table player (
     account_balance int null,
     roles varchar(50) null,
     hole_cards varchar(150) null,
-    position varchar(50) null,
+    position varchar(150) null,
     is_player_action bit null
 );
 
 create table board (
 	board_id int primary key auto_increment,
     flop varchar(50) null,
-    turn varchar(100) not null,
-    river varchar(200) not null
+    turn varchar(100) null,
+    river varchar(200) null
 );
 
 create table game (
 	game_id int primary key auto_increment,
     pot int null,
     winner varchar(150) null,
-    board_id int not null,
-    player_one_id int not null,
-    player_two_id int not null,
+    last_action varchar(50) null,
+    board_id int null,
+    player_one_id int null,
+    player_two_id int null,
 	constraint fk_game_board_id
 		foreign key (board_id)
         references board(board_id),
@@ -90,15 +91,15 @@ begin
 	insert into `role` (`name`) values
 		('USER'),
 		('ADMIN');
-		
-	insert into player (player_id, username, password_hash, enabled, roles)
+    
+	insert into player (player_id, username, password_hash, display_name, account_balance, enabled, roles, hole_cards, `position`, is_player_action)
 		values
-		(1, 'john@smith.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'USER'),
-		(2, 'sally@jones.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'USER'),
-        (3, 'fred@astair.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'USER'),
-        (4, 'billy@bob.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'ADMIN'),
-        (5, 'sam@stone.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'USER'),
-        (6, 'lisa@simpson.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 1, 'USER');
+		(1, 'john@smith.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 100, 1, 'USER', 'AH,KD', 'SMALLBLIND', true),
+		(2, 'sally@jones.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 50, 1, 'USER', 'JC,9H', 'BIGBLIND', true),
+        (3, 'fred@astair.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 25, 1, 'USER', '8S,6S', 'SMALLBLIND', false),
+        (4, 'billy@bob.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 80, 1, 'ADMIN', '7H,2D', 'SMALLBLIND', true),
+        (5, 'sam@stone.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 150, 1, 'USER', 'AC,5D', 'BIGBLIND', true),
+        (6, 'lisa@simpson.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', null, 225, 1, 'USER', null, 'BIGBLIND', false);
         
 	insert into player_role (player_id, role_id)
 		values
@@ -115,17 +116,18 @@ begin
         (2, '10C,3D,QS', 'AC', '5S'),
         (3, '7H,QS,8S', '6D', '2D');
 		
-	insert into game (game_id, pot, winner, board_id, player_one_id, player_two_id)
+	insert into game (game_id, pot, winner, last_action, board_id, player_one_id, player_two_id)
 		values
-		(1, 200, 'sally@jones.com', 3, 1, 2),
-        (2, 250, 'fred@astair.com', 2, 3, 4),
-        (3, 300, 'john@smith.com', 1, 5, 6);
+		(1, 200, 'sally@jones.com', 'RAISE', 3, 1, 2),
+        (2, 250, 'fred@astair.com', 'FOLD', 2, 3, 4),
+        (3, 300, 'john@smith.com', 'CHECK', 1, 5, 6);
 	 
 	 insert into room (room_id, stake, seats, game_id) 
 		values 
 		(1, 0.25, 2, 2),
         (2, 0.5, 2, 3),
-        (3, 0.75, 2, 1);
+        (3, 0.75, 2, 1),
+        (4, 2.00, 2, null);
 	
     set sql_safe_updates = 1;
     
