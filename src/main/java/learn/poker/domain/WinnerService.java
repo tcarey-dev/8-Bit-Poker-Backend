721a62f2d845e5c3f51704fc11fa4ba18494779e
aaronsocks <aasaccurato@gmail.com>
2023-07-03 09:58:11 -0700

    started winner service

--- src/main/java/learn/poker/domain/WinnerService.java
+++ src/main/java/learn/poker/domain/WinnerService.java
@@ -3,6 +3,7 @@ package learn.poker.domain;
 import learn.poker.models.Player;
 import learn.poker.models.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,13 +11,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WinnerService {
     @Autowired
     private WebClient.Builder client;
-    private static String api_url = "https://api.pokerapi.dev/v1/winner/texas_holdem?";
+    private static String api_url = "https://api.pokerapi.dev/v1/winner/texas_holdem";
 
     public String determineWinner(Room room) {
-//        String.format("%?cc=%s,%s,%s,%s,%s&pc[]=%s,%s&pc=[]%s%s",   )
-        return null;
+
+        return client.build().get()
+                .uri(String.format("%s?cc=%s,%s,%s,%s,%s&pc[]=%s,%s&pc=[]%s%s", api_url,
+                        room.getGame().getBoard().getFlop().get(0)),
+                        room.getGame().getBoard().getFlop().get(1),
+                        room.getGame().getBoard().getFlop().get(2),
+                        room.getGame().getBoard().getTurn(),
+                        room.getGame().getBoard().getRiver(),
+                        room.getGame().getPlayers().get(0).getHoleCards().get(0),
+                        room.getGame().getPlayers().get(0).getHoleCards().get(1),
+                        room.getGame().getPlayers().get(1).getHoleCards().get(0),
+                        room.getGame().getPlayers().get(1).getHoleCards().get(1))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
-
-
 
 }
