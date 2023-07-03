7e24c16b1a3739129f899a2768d8de405f494806
aaronsocks <aasaccurato@gmail.com>
2023-07-02 19:26:23 -0700

    added xiao portions of test

--- src/test/java/learn/poker/domain/GameServiceGameLogicTest.java
+++ src/test/java/learn/poker/domain/GameServiceGameLogicTest.java
@@ -1,6 +1,7 @@
 package learn.poker.domain;
 
 import learn.poker.data.GameRepository;
+import learn.poker.data.PlayerRepository;
 import learn.poker.data.RoomRepository;
 import learn.poker.models.*;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,9 @@ public class GameServiceGameLogicTest {
     @MockBean
     RoomRepository roomRepository;
 
+    @MockBean
+    PlayerRepository playerRepository;
+
     @Test
     void shouldInitGame(){
         Room room = new Room(4, 2, 2, null);
@@ -49,52 +53,250 @@ public class GameServiceGameLogicTest {
 
     // init
     @Test
-    void shouldNotInitRoomWithNullId(){}
+    void shouldNotInitNonExistingRoom(){
+        Room room = new Room(9999, 2, 2, null);
 
-    @Test
-    void shouldNotInitNonExistingRoom(){}
+        Result<Room> result = gameService.init(room);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getMessages().size());
+        assertTrue(result.getMessages().get(0).contains("not found"));
+    }
 
 
     // update i.e. add players
     @Test
-    void shouldUpdateRoomToHaveOnePlayer(){}
+    void shouldUpdateGameToHaveOnePlayer(){
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+
+        Board board = new Board(List.of(Card.QUEEN_OF_HEARTS, Card.ACE_OF_SPADES, Card.ACE_OF_DIAMONDS), Card.KING_OF_DIAMONDS, Card.EIGHT_OF_CLUBS);
+
+        Game game = new Game();
+        game.setGameId(4);
+        game.setPot(0);
+        game.setWinner(null);
+        game.setBetAmount(0);
+        game.setLastAction(Action.NONE);
+        game.setBoard(board);
+        game.setPlayers(List.of(player1));
+
+        Room room = new Room(4, 2, 2, game);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Game> result = gameService.update(game);
+
+        assertTrue(result.isSuccess());
+        assertEquals(1, game.getPlayers().size());
+        assertEquals(1, game.getPlayers().get(0).getPlayerId());
+    }
 
     @Test
-    void shouldUpdateRoomToHaveTwoPlayers(){}
+    void shouldUpdateGameToHaveTwoPlayers(){
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        Player player2 = new Player(2,"player2Username", "password",true, List.of("USER"));
+        Board board = new Board(List.of(Card.QUEEN_OF_HEARTS, Card.ACE_OF_SPADES, Card.ACE_OF_DIAMONDS), Card.KING_OF_DIAMONDS, Card.EIGHT_OF_CLUBS);
 
-    @Test
-    void shouldNotUpdateRoomToHavePlayersIfGameIsNull(){}
+        Game game = new Game();
+        game.setGameId(4);
+        game.setPot(0);
+        game.setWinner(null);
+        game.setBetAmount(0);
+        game.setLastAction(Action.NONE);
+        game.setBoard(board);
+        game.setPlayers(List.of(player1, player2));
+
+        Room room = new Room(4, 2, 2, game);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Game> result = gameService.update(game);
+
+        assertTrue(result.isSuccess());
+        assertEquals(2, game.getPlayers().size());
+        assertEquals(1, game.getPlayers().get(0).getPlayerId());
+        assertEquals(2, game.getPlayers().get(1).getPlayerId());
+
+    }
+
 
     // start
     @Test
     void shouldStartGameWithTwoPlayers(){
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        Player player2 = new Player(2,"player2Username", "password",true, List.of("USER"));
+        player1.setAccountBalance(100);
+        player2.setAccountBalance(100);
+        Board board = new Board(List.of(Card.QUEEN_OF_HEARTS, Card.ACE_OF_SPADES, Card.ACE_OF_DIAMONDS), Card.KING_OF_DIAMONDS, Card.EIGHT_OF_CLUBS);
 
+        Game game = new Game();
+        game.setGameId(4);
+        game.setPot(0);
+        game.setWinner(null);
+        game.setBetAmount(0);
+        game.setLastAction(Action.NONE);
+        game.setBoard(board);
+        game.setPlayers(List.of(player1, player2));
+
+        Room room = new Room(4, 2, 2, game);
+
+        Result<Room> result = gameService.start(room);
+
+        assertTrue(result.isSuccess());
     }
     @Test
-    void shouldNotStartGameWithLessThanTwoPlayers(){}
+    void shouldNotStartGameWithLessThanTwoPlayers(){
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        Board board = new Board(List.of(Card.QUEEN_OF_HEARTS, Card.ACE_OF_SPADES, Card.ACE_OF_DIAMONDS), Card.KING_OF_DIAMONDS, Card.EIGHT_OF_CLUBS);
+
+        Game game = new Game();
+        game.setGameId(4);
+        game.setPot(0);
+        game.setWinner(null);
+        game.setBetAmount(0);
+        game.setLastAction(Action.NONE);
+        game.setBoard(board);
+        game.setPlayers(List.of(player1));
+
+        Room room = new Room(4, 2, 2, game);
+
+        Result<Room> result = gameService.start(room);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getMessages().size());
+        assertEquals("Game must have two players to start.", result.getMessages().get(0));
+    }
 
     @Test
-    void shouldNotStartGameIfEitherPlayerAccountBalanceIsLessThanOrEqualToZero(){}
+    void shouldNotStartGameIfEitherPlayerAccountBalanceIsLessThanOrEqualToZero(){
+        //this needs to be tested b/c a player can have an account balance of 0 or less after a game and thus
+        //another game cannot be started when this happens
+
+        Player player1 = new Player(0,"username", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa", true, List.of("USER"));
+        player1.setAccountBalance(0.0);
+
+        Player player2 = new Player(0,"username", "$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa", true, List.of("USER"));
+        player2.setAccountBalance(0.0);
+
+        when(playerRepository.create(player1)).thenReturn(player1);
+        when(playerRepository.create(player2)).thenReturn(player2);
+
+        Game game = new Game(0, 0, null, 0, null, null, null);
+        Game expectedGame = new Game();
+        expectedGame.setGameId(4);
+        expectedGame.setPot(0);
+        expectedGame.setWinner(null);
+        expectedGame.setBetAmount(0);
+        expectedGame.setLastAction(Action.NONE);
+        expectedGame.setBoard(null);
+        expectedGame.setPlayers(List.of(player1, player2));
+
+        Room room = new Room(4, 2, 2, expectedGame);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Room> result = gameService.start(room);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getMessages().size());
+        assertEquals("Account Balance must be greater than 0 to start game.", result.getMessages().get(0));
+    }
 
     @Test
-    void startGameShouldSetLastActionToNone() {}
+    void startGameShouldSetLastActionToNone() {
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        Player player2 = new Player(2,"player2Username", "password",true, List.of("USER"));
+        player1.setAccountBalance(100);
+        player2.setAccountBalance(100);
+
+        Game game = new Game(0, 0, null, 0, null, null, null);
+        Game expectedGame = new Game();
+        expectedGame.setGameId(4);
+        expectedGame.setPot(0);
+        expectedGame.setWinner(null);
+        expectedGame.setBetAmount(0);
+        expectedGame.setLastAction(Action.NONE);
+        expectedGame.setBoard(null);
+        expectedGame.setPlayers(List.of(player1, player2));
+
+        Room room = new Room(4, 2, 2, expectedGame);
+
+        when(playerRepository.create(player1)).thenReturn(player1);
+        when(playerRepository.create(player2)).thenReturn(player2);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Room> result = gameService.start(room);
+
+        assertTrue(result.isSuccess());
+        assertEquals(Action.NONE,result.getPayload().getGame().getLastAction());
+    }
 
     @Test
-    void startGameShouldSetPlayerOnePositionToSmallBlind() {}
+    void startGameShouldSetPlayerOnePositionToSmallBlind() {
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        Player player2 = new Player(2,"player2Username", "password",true, List.of("USER"));
+        player1.setAccountBalance(100);
+        player2.setAccountBalance(100);
+
+        Game game = new Game(0, 0, null, 0, null, null, null);
+        Game expectedGame = new Game();
+        expectedGame.setGameId(4);
+        expectedGame.setPot(0);
+        expectedGame.setWinner(null);
+        expectedGame.setBetAmount(0);
+        expectedGame.setLastAction(Action.NONE);
+        expectedGame.setBoard(null);
+        expectedGame.setPlayers(List.of(player1, player2));
+
+        Room room = new Room(4, 2, 2, expectedGame);
+
+        when(playerRepository.create(player1)).thenReturn(player1);
+        when(playerRepository.create(player2)).thenReturn(player2);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Room> result = gameService.start(room);
+
+        assertTrue(result.isSuccess());
+        assertEquals(Position.SMALLBLIND, player1.getPosition());
+    }
 
     @Test
-    void startGameShouldSetSetPlayerTwoPositionToBigBlind() {}
+    void startGameShouldSetSetPlayerTwoPositionToBigBlind() {
+        Player player1 = new Player(1,"player1Username", "password",true, List.of("USER"));
+        player1.setAccountBalance(100);
+        Player player2 = new Player(2,"player2Username", "password",true, List.of("USER"));
+        player2.setAccountBalance(100);
 
-    // should subtract blinds correctly from each players account balance
-    // should add blinds to pot
-    // should add two cards to each players holeCards field
-    // should set Smallblind playersAction to true
-    // should set big blind playersAction to false
+        Game game = new Game(0, 0, null, 0, null, null, null);
+        Game expectedGame = new Game();
+        expectedGame.setGameId(4);
+        expectedGame.setPot(0);
+        expectedGame.setWinner(null);
+        expectedGame.setBetAmount(0);
+        expectedGame.setLastAction(Action.NONE);
+        expectedGame.setBoard(null);
+        expectedGame.setPlayers(List.of(player1, player2));
 
+        Room room = new Room(4, 2, 2, expectedGame);
 
-    // handleAction fold
-    @Test
-    void foldActionShouldSetOpponentAsWinner(){} //xiao does this one up
+        when(playerRepository.create(player1)).thenReturn(player1);
+        when(playerRepository.create(player2)).thenReturn(player2);
+
+        when(gameRepository.update(game)).thenReturn(true);
+        when(roomRepository.update(room)).thenReturn(true);
+
+        Result<Room> result = gameService.start(room);
+
+        assertTrue(result.isSuccess());
+        assertEquals(Position.BIGBLIND, player2.getPosition());}
+
 
     @Test
     void foldActionShouldAddPotToWinnersAccountBalance(){ //aaron start here
